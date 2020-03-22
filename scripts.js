fc24241bdcde2ce32a5a985e269f5c43252aabfc
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-03-22 11:49:15 +0000

    add animations

--- scripts.js
+++ scripts.js
@@ -14,4 +14,42 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+
+  if( $(window).width() < 600 ) {
+   $('name').removeClass('neon');
+  }
+
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('projects'),
+    handler: function() {
+   
+      if ($(window).width() > 600) {
+      $('.hide').fadeIn(1500).removeClass('hide');
+      $('.hide-img').fadeIn(1500).removeClass('.hide-img');
+      }
+      else {
+        $('.hide').fadeIn(1500).removeClass('hide');
+      }
+    },
+    offset: 340
+  });
+
+  var waypoint2 = new Waypoint({
+    element: document.getElementById('projects2'),
+    handler: function() {
+    
+      if ($(window).width() > 600) {
+      $('.hide2').fadeIn(1500).removeClass('hide2');
+      $('.hide-img2').fadeIn(1500).removeClass('.hide-img2');
+      }
+      else {
+        $('.hide2').fadeIn(1500).removeClass('hide2');
+      }
+    },
+    offset: 340
+  });
+
+
+
 });
